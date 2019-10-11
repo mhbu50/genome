412fafc0fe069ac2162fb276781a9f6cb3081174
Your Name <you@example.com>
2019-10-11 11:23:34 +0300

    change envelop to hash id

--- genome/genome/tool.py
+++ genome/genome/tool.py
@@ -30,58 +30,40 @@ def add_pdf(doc):
 
 
 def execute(doc):
-    envelop_name = frappe.generate_hash(length=34)
     shareable_file_name = frappe.generate_hash(length=34)
     lab_test_doc = frappe.get_doc("Lab Test", doc)
-    lab_test_doc.envelope_id = envelop_name
     lab_test_doc.save()
-    
-
     if not frappe.db:
         frappe.connect()
-
-    # upload database
     dropbox_settings = get_dropbox_settings()
-
     if not dropbox_settings['access_token']:
         access_token = generate_oauth2_access_token_from_oauth1_token(
             dropbox_settings)
-
         if not access_token.get('oauth2_token'):
             return 'Failed backup upload', 'No Access Token exists! Please generate the access token for Dropbox.'
-
         dropbox_settings['access_token'] = access_token['oauth2_token']
         set_dropbox_access_token(access_token['oauth2_token'])
-    print"\n lab_test_doc.envelope_id = {}".format(lab_test_doc.envelope_id)
     dropbox_client = dropbox.Dropbox(dropbox_settings['access_token'])
-    # print "befor upload"
     upload_file_to_dropbox(get_file_path(
         lab_test_doc.lab_test_result_file[7:]), DROPBOX_TARGET, dropbox_client)
     upload_file_to_dropbox(get_file_path(
         lab_test_doc.arabic_result_file[7:]), DROPBOX_TARGET, dropbox_client)
-
     html_data1 = frappe.render_template("templates/envlop.html",
                                         {"patient_name": lab_test_doc.arabic_first_name, "disease": lab_test_doc.disease_description, "file1": lab_test_doc.lab_test_result_file[7:], "file2": lab_test_doc.arabic_result_file[7:],
                                          "shareable_file_name": shareable_file_name, "hash_id": lab_test_doc.hash_id})
-    envlop_file = save_generated_file(html_data1, envelop_name)
+    envlop_file = save_generated_file(html_data1, lab_test_doc.hash_id)
     upload_file_to_dropbox(get_file_path(envlop_file), DROPBOX_TARGET, dropbox_client)
 
     disease_doc = frappe.get_doc("Diseases", lab_test_doc.disease)
-    html_data2 = frappe.render_template("templates/shareable_file1.html",
+    html_data2 = frappe.render_template("templates/shareable_file.html",
                                         {"disease": disease_doc.name,
                                          "disease_story": disease_doc.description,
                                          "html_pattern": disease_doc.html_pattern,
                                          "hash_id": lab_test_doc.hash_id})
-
     shareable_file = save_generated_file(html_data2, shareable_file_name)
     upload_file_to_dropbox(get_file_path(shareable_file),
                            DROPBOX_TARGET, dropbox_client)
     frappe.db.close()
-    # print "after upload"
-    # print("envelop_name = {}".format(envelop_name))
-    # print("shareable_file_name = {}".format(shareable_file_name))
-
-    return "uploaded"
 
 
 def get_html_data(doctype, name):
@@ -93,7 +75,6 @@ def get_html_data(doctype, name):
 def save_generated_file(content, to_name):
     from frappe.utils.file_manager import save_file_on_filesystem
     file_name = "{}.html".format(to_name.replace(" ", "-").replace("/", "-"))
-    # print " to_name = {} ".format(to_name)
     save_file_on_filesystem(file_name, content, None, is_private=0)
     return file_name
 
