fb84165f20ae1c2166aafa9627dc8ec0c1243e10
mhbu50 <mhbu50@gmail.com>
2020-05-27 23:15:11 +0300

    add action log

---
+++ genome/genome/doctype/customer_action_log/customer_action_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Accurate Systems and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Action Log', {
+	refresh: function(frm) {
+
+	}
+});
