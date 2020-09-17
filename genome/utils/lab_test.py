74ef3fc90fb3aab5a59354eae0677501cca58a0b
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2020-09-17 14:55:37 +0500

    code refactored:   Lab Test

--- genome/utils/lab_test.py
+++ genome/utils/lab_test.py
@@ -9,10 +9,17 @@ def generate_sales_invoice(doc, method):
     if doc.sales_invoice: return
     item_code, lab_test_rate = frappe.get_value('Lab Test Template', doc.template, ['item', 'lab_test_rate'])
     sales_invoice = frappe.new_doc("Sales Invoice")
-    sales_invoice.patient = doc.patient
-    sales_invoice.patient_name = doc.patient_name
-    sales_invoice.ref_practitioner = doc.practitioner
-    sales_invoice.customer_name_in_arabic = doc.arabic_first_name
+    doc_map = {
+        'patient' : 'patient',
+        'patient_name': 'patient_name',
+        'ref_practitioner': 'practitioner',
+        'customer_name_in_arabic': 'arabic_first_name',
+        'national_id': 'national_id'
+    }
+    for key, value in doc_map.items():
+        setattr(sales_invoice, key, 
+        getattr(doc, value))
+    
     sales_invoice.customer = frappe.get_value('Patient', doc.patient, "customer")
     sales_invoice.append("items", {
         "item_code": item_code,
