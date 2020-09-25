c49dc249e2f3b5f3f9b47f13a5f6d44a897f16d2
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2020-09-25 11:20:11 +0500

    feat:   Sales Invoice     send invoice link for the selecting print format via sms

--- genome/api/sales_invoice.py
+++ genome/api/sales_invoice.py
@@ -1,5 +1,22 @@
 import frappe
+from frappe.core.doctype.communication.email import get_attach_link
+from frappe.utils import get_url
+from frappe.core.utils import get_parent_doc
+from requests.utils import requote_uri
 
 @frappe.whitelist()
 def set_access_token(name, token):
     frappe.db.set_value('Sales Invoice', name, 'access_token', token)
+
+
+@frappe.whitelist()
+def get_attachment_link(doc, print_format):
+    doc = frappe.get_doc('Sales Invoice', doc)
+    setattr(doc, 'reference_doctype', 'Sales Invoice')
+    setattr(doc, 'reference_name', doc.name)
+
+    key = doc.get_signature()
+
+    link = f'{ get_url() }/{ doc.doctype }/{ doc.name }?format={ print_format}&key={ key }'
+    link = requote_uri(link)
+    return link
--- genome/public/js/sales_invoice.js
+++ genome/public/js/sales_invoice.js
@@ -2,14 +2,25 @@ frappe.ui.form.on('Sales Invoice',{
     refresh: function (frm) {  
         if (frm.doc.docstatus == 1){
             frm.add_custom_button('Send Message', () => {
-				frm.trigger("show_send_media_dialog");
+                frappe.db.get_list('Print Format', {
+                    fields: ['name', 'standard'],
+                    filters: {
+                        doc_type: 'Sales Invoice'
+                    }
+                }).then(records => {
+                    let print_formats = ""
+                    for (let i in records){
+                        print_formats +=`\n${records[i].name}`
+                    }
+                    cur_frm.doc.d_print_format_options = print_formats
+                    frm.trigger("show_send_media_dialog");
+                })
             })
         }
     },
     show_send_media_dialog: function (frm) {  
         if (cur_frm.doc.d){
             cur_frm.doc.d.show();
-            // frm.trigger("set_reset_message_button");
         }else{
             let new_fields = []
             new_fields.push({   
@@ -26,6 +37,13 @@ frappe.ui.form.on('Sales Invoice',{
                 "depends_on": `eval: ["SMS","WhatsApp"].includes(doc.media_type)`,
                 "read_only": 1})
             
+            new_fields.push({
+                "label": __("Print Format"),
+                "fieldname": "print_format",
+                "fieldtype": "Select",
+                "options": cur_frm.doc.d_print_format_options,
+                "reqd": 1})
+            
             new_fields.push({   
                 "label": __("Message Template"),
                 "fieldname": "template",
@@ -33,11 +51,18 @@ frappe.ui.form.on('Sales Invoice',{
                 "options": "SMS Template",
                 "reqd": 1,
                 onchange: function () {
-                    var args = cur_frm.doc.d.get_values();
+                    let args = cur_frm.doc.d.get_values();
                     if (args.template != undefined) {
                         frappe.db.get_doc('SMS Template', args.template).then(doc => {
-                            cur_frm.doc.d.set_value('message', doc.sms_text);
-                            cur_frm.trigger("set_token");
+                            frappe.call("genome.api.sales_invoice.get_attachment_link", {
+                                doc: cur_frm.doc.name,
+                                print_format: args.print_format
+                            }).then(r =>{
+                                let doc_args = cur_frm.doc
+                                args.message = frappe.render_template(`${doc.sms_text}`, {doc : doc_args}); 
+                                args.message += '\n\n' + r.message
+                                cur_frm.doc.d.set_value('message', args.message);
+                            })
                         })
                     }
                 }})
@@ -51,7 +76,7 @@ frappe.ui.form.on('Sales Invoice',{
             cur_frm.doc.d = new frappe.ui.Dialog({
                 fields: new_fields,
                 primary_action: function (e) {
-                    var v = cur_frm.doc.d.get_values();
+                    let v = cur_frm.doc.d.get_values();
                     frappe.call("frappe.core.doctype.sms_settings.sms_settings.send_sms",
                     {receiver_list: [v.mobile], msg: v.message})
                     .then(r =>{
@@ -66,64 +91,5 @@ frappe.ui.form.on('Sales Invoice',{
             cur_frm.doc.d.show()
 
         }
-    },
-    set_token: function () {  
-        var args = cur_frm.doc.d.get_values();
-
-        frappe.db.get_list("SMS Token",{
-            fields: ["token", "days"],
-            filters: {
-                docstatus: 1,
-                docname: cur_frm.docname,
-                document_type: cur_frm.doctype
-            }
-        }).then( records =>{
-            if (records.length > 0){
-                var token = records[0].token;
-                frappe.db.get_doc('SMS Template', args.template).then(doc => {
-                    cur_frm.doc.d.set_value('message', doc.sms_text);
-                }).then(r =>{
-                    args.message = args.message.replace("{{token}}", token);
-                    var doc_args = cur_frm.doc
-                    args.message = frappe.render_template(`${args.message}`, {doc : doc_args}); 
-                    cur_frm.doc.d.set_value("message", args.message);
-                    if (!cur_frm.doc.access_token){
-                        frappe.call('genome.api.sales_invoice.set_access_token',
-                        {name: cur_frm.doc.name, token: token})
-                        .then(r =>{
-                            cur_frm.reload_doc()
-                        })
-                    }
-                })
-            }else{
-                var token = frappe.utils.get_random(16)
-                frappe.db.get_doc('SMS Template', args.template).then(doc => {
-                    cur_frm.doc.d.set_value('message', doc.sms_text);
-                }).then(r =>{
-                    args.message = args.message.replace("{{token}}", token);
-                    var doc_args = cur_frm.doc
-                    args.message = frappe.render_template(`${args.message}`, {doc : doc_args}); 
-        
-                    frappe.db.insert({
-                        doctype: 'SMS Token',
-                        token: token,
-                        docname: cur_frm.docname,
-                        document_type: cur_frm.doctype,
-                        days: 10,
-                        docstatus: 1
-        
-                    }).then(doc => {
-                        cur_frm.doc.d.set_value("message", args.message);
-                        if (!cur_frm.doc.access_token){
-                            frappe.call('genome.api.sales_invoice.set_access_token',
-                            {name: cur_frm.doc.name, token: token})
-                            .then(r =>{
-                                cur_frm.reload_doc()
-                            })
-                        }
-                    })
-                })
-            }
-        })
     }
 })
