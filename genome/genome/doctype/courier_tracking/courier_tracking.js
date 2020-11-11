619a228e8f0bae29460a3c453b2f976622194d12
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2020-11-11 11:44:22 +0500

    Query: Courier Tracking

--- genome/genome/doctype/courier_tracking/courier_tracking.js
+++ genome/genome/doctype/courier_tracking/courier_tracking.js
@@ -3,6 +3,15 @@
 
 frappe.ui.form.on('Courier Tracking', {
 	refresh: function(frm) {
-
+		frm.events.set_query(frm);
+	},
+	set_query: function (frm){
+		frm.set_query('courier', () => {
+			return {
+				filters: {
+					'supplier_group': 'Courier'
+				}
+			}
+		})
 	}
 });
