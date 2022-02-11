65ef7a8a8981baa20736c4c907da8c3478608eb1
rizwanpak <rizwan.shahid@havenir.com>
2022-02-11 19:10:20 +0500

    fix( patient ): 1- creating user for patient depending upon the Genome Settings.

---
+++ genome/genome/doctype/genome_settings/genome_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Accurate Systems and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class GenomeSettings(Document):
+	pass
