be3608a58ecfaced96017ce26e091bf42ef903fd
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2022-05-18 18:04:29 +0200

    fix: custom field in lab test

--- genome/public/js/lab_test.js
+++ genome/public/js/lab_test.js
@@ -40,10 +40,10 @@ frappe.ui.form.on('Lab Test', {
             frm.trigger('generate_buttons')
             
             // Work around to attach files to a field on update after submit
-            frappe.call('genome.api.lab_test.update_lab_result_file_link', {docname: frm.doc.name})
-            .then(r =>{
-                frm.refresh_field('lab_result_file')
-            })
+            // frappe.call('genome.api.lab_test.update_lab_result_file_link', {docname: frm.doc.name})
+            // .then(r =>{
+            //     frm.refresh_field('lab_result_file')
+            // })
         }
     },
     create_sales_invoice_button(frm){
